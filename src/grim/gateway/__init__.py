c1a237742028ce3635afd2024946362923c07984
toast <cpastoast@protonmail.com>
2022-05-25 14:59:13 +0200

    fix

--- src/grim/gateway/__init__.py
+++ src/grim/gateway/__init__.py
@@ -12,12 +12,10 @@ class WebSocket(websocket.WebSocket):
 
     def recv_json(self) -> Dict:
         """Receive a JSON object from the server"""
-        response = self.recv()
 
-        if response is None:
-            return {}
-
-        return json.loads(response)
+        return (
+            {} if (response := self.recv()) is None else json.loads(response)
+        )
 
     def send_json(self, data: Dict) -> None:
         """Send a JSON object to the server"""
@@ -34,37 +32,36 @@ class Gateway:
         self.connected = False
 
     def connect(self) -> None:
-        self.connected = True
 
         self.ws.connect("wss://gateway.discord.gg/?v=6&encoding=json")
-
-        payload = {
-            "op": 2,
-            "d": {
-                "token": self.token,
-                "properties": {
-                    "$os": "linux",
-                    "$browser": "chrome",
-                    "$device": "pc",
+        self.ws.send_json(
+            {
+                "op": 2,
+                "d": {
+                    "token": self.token,
+                    "properties": {
+                        "$os": "linux",
+                        "$browser": "chrome",
+                        "$device": "pc",
+                    },
                 },
-            },
-        }
+            }
+        )
 
-        self.ws.send_json(payload)
+        self.connected = True
 
     def listen(self) -> Generator[Payload, None, None]:
         """Listen for events"""
-
-        if self.connected is False:
-            self.connect()
-
         # weird way to re-connect to the WS, gotta fix this ASAP
         while True:
+
+            if self.connected is False:
+                self.connect()
+
             try:
                 data = self.ws.recv_json()
             except websocket._exceptions.WebSocketConnectionClosedException:
                 self.connected = False
-                self.connect()
                 continue
 
             with contextlib.suppress(json.decoder.JSONDecodeError):
